685d4c465ff2f84a326a3c59eee2d452713d0c46
Petr Spacek <pspacek@redhat.com>
2016-10-04 14:07:46 +0200

    rename directory le->ca to make it self-explanatory

--- setup-le.sh
+++ setup-le.sh
@@ -4,10 +4,10 @@ set -o nounset -o errexit
 # do this once before taking snapshot of the VM
 dnf install letsencrypt -y
 
-ipa-cacert-manage install /root/le/DSTRootCAX3.pem -n DSTRootCAX3 -t ,,
+ipa-cacert-manage install /root/ca/DSTRootCAX3.pem -n DSTRootCAX3 -t ,,
 ipa-certupdate -v
 
 # this command might blow up, Honza knows a workaround - it is a bug :-)
-ipa-cacert-manage install /root/le/LetsEncryptAuthorityX1.pem -n letsencrypt -t C,,
+ipa-cacert-manage install /root/ca/LetsEncryptAuthorityX1.pem -n letsencrypt -t C,,
 ipa-certupdate -v
 
