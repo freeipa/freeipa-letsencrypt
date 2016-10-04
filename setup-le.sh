2b1ca27595e90b743b341ac18e2c6004e7940562
Petr Spacek <pspacek@redhat.com>
2016-10-04 14:11:06 +0200

    use official Let's Encrypt root CA certificate

--- setup-le.sh
+++ setup-le.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/bash
 set -o nounset -o errexit
 
-# do this once before taking snapshot of the VM
+WORKDIR="/root/ipa-le"
+
 dnf install letsencrypt -y
 
-ipa-cacert-manage install /root/ca/DSTRootCAX3.pem -n DSTRootCAX3 -t ,,
+ipa-cacert-manage install "$WORKDIR/ca/isrgrootx1.pem" -n ISRGRootX1 -t ,,
 ipa-certupdate -v
 
-# this command might blow up, Honza knows a workaround - it is a bug :-)
-ipa-cacert-manage install /root/ca/LetsEncryptAuthorityX1.pem -n letsencrypt -t C,,
+ipa-cacert-manage install "$WORKDIR/ca/LetsEncryptAuthorityX3.pem" -n letsencryptx3 -t C,,
 ipa-certupdate -v
 
 "$(dirname "$0")/renew-le.sh" "--first-time"
