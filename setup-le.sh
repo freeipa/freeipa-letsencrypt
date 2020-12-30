03761ef9a3e58d957250bde97748eed74091c3f8
Mr. Snrub <22333421+afrothundaaaa@users.noreply.github.com>
2020-12-30 01:13:34 +0000

    Update setup-le.sh to dynamically retrieve LE CA chain
    Detect wget or curl availability and retrieve the set of current certs
    from LE directly and import them into IPA.
    
    Use the current hostname to configure the CSR for the LE
    certificate request.
    
    Fixes: https://github.com/freeipa/freeipa-letsencrypt/issues/25
    Signed-off-by: Mr. Snrub <22333421+afrothundaaaa@users.noreply.github.com>

--- setup-le.sh
+++ setup-le.sh
@@ -1,14 +1,31 @@
 #!/usr/bin/bash
 set -o nounset -o errexit
 
+FQDN=$(hostname -f)
 WORKDIR=$(dirname "$(realpath $0)")
+CERTS=("isrgrootx1.pem" "isrg-root-x2.pem" "lets-encrypt-r3.pem" "lets-encrypt-e1.pem" "lets-encrypt-r4.pem" "lets-encrypt-e2.pem")
+
+sed -i "s/server.example.test/$FQDN/g" $WORKDIR/ipa-httpd.cnf
 
 dnf install letsencrypt -y
 
-ipa-cacert-manage install "$WORKDIR/ca/DSTRootCAX3.pem" -n DSTRootCAX3 -t C,,
-ipa-certupdate -v
+if [ ! -d "/etc/ssl/$FQDN" ]
+then
+  mkdir -p "/etc/ssl/$FQDN"
+fi
 
-ipa-cacert-manage install "$WORKDIR/ca/LetsEncryptAuthorityX3.pem" -n letsencryptx3 -t C,,
-ipa-certupdate -v
+for CERT in "${CERTS[@]}"
+do
+  if command -v wget &> /dev/null
+  then
+    wget -O "/etc/ssl/$FQDN/$CERT" "https://letsencrypt.org/certs/$CERT"
+  elif command -v curl &> /dev/null
+  then
+    curl -o "/etc/ssl/$FQDN/$CERT" "https://letsencrypt.org/certs/$CERT"
+  fi
+  ipa-cacert-manage install "/etc/ssl/$FQDN/$CERT"
+done
+
+ipa-certupdate
 
 "$WORKDIR/renew-le.sh" --first-time
